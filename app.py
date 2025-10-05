3d41ff5841ebf2c32df11da31e880faba559fd0d
s64864316 <s64864316@gmail.com>
2025-10-05 00:17:49 -0300

    a

--- app.py
+++ app.py
@@ -771,5 +771,4 @@ def contacts():
     return render_template('contacts.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
